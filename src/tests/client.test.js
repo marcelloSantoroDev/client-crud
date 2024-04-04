d91f916b03ef66db95fddab25be00b89e45b8c3f
Marcello <marcelloaltermundi@gmail.com>
2024-04-04 20:17:05 -0300

    first push

--- src/tests/client.test.js
+++ src/tests/client.test.js
@@ -1,77 +1,74 @@
-const sinon = require('sinon');
-const chai = require('chai');
-const { expect } = chai;
-const chaiHttp = require('chai-http');
-const clientModel = require('../models/clients');
-const { getClients, getClient } = require('./mocks');
-
-
-const app = require('../../src/app');
+import sinon from 'sinon';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import clientModel from '../models/clients';
+import { getClients, getClient } from './mocks';
+import app from '../../src/app';
 
 chai.use(chaiHttp);
 
-describe('testa a rota GET /clients', async function () {
+const { expect } = chai;
+
+describe('testa a rota GET /clients', function () {
     afterEach(function () {
         sinon.restore();
     });
 
     let chaiHttpResponse;
     it('testa se é possível listar todos os clientes', async function () {
-
         sinon
             .stub(clientModel, "findAll")
             .resolves(getClients);
 
-
-
         chaiHttpResponse = await chai.request(app).get('/clients');
-        expect(response.status).to.be.equal(200);
+        expect(chaiHttpResponse.status).to.be.equal(200);
         expect(chaiHttpResponse.body).to.be.deep.equal(getClients);
-
     });
-    it('testa se é possível listar um cliente específico', async function () {
 
+    it('testa se é possível listar um cliente específico', async function () {
         sinon
             .stub(clientModel, "findOne")
             .resolves(getClient);
-
 
         chaiHttpResponse = await chai.request(app).get('/clients/1');
-        expect(response.status).to.be.equal(200);
-        expect(chaiHttpResponse.body).to.be.deep.equal(getClients);
-
+        expect(chaiHttpResponse.status).to.be.equal(200);
+        expect(chaiHttpResponse.body).to.be.deep.equal(getClient);
     });
-    it('testa erro ao listar um cliente específico', async function () {
 
+    it('testa erro ao listar um cliente específico', async function () {
         sinon
             .stub(clientModel, "findOne")
-            .resolves(getClient);
-
+            .resolves(null);
 
         chaiHttpResponse = await chai.request(app).get('/clients/2');
-        expect(response.status).to.be.equal(400);
-        expect(chaiHttpResponse.body).to.be.deep.equal({"message": "Cliente não encontrado"});
-
-    })
+        expect(chaiHttpResponse.status).to.be.equal(400);
+        expect(chaiHttpResponse.body).to.be.deep.equal({ "message": "Cliente não encontrado" });
+    });
 });
 
-describe('Testa a rota POST de /clients', async function () {
-    afterEach(function () {
-        sinon.restore();
-    });
-
+describe('Testa a rota POST de /clients', function () {
     it('testa se é possível cadastrar um cliente', async function () {
         sinon
             .stub(clientModel, "create")
             .resolves(getClient);
 
-        chaiHttpResponse = await chai.request(app).post('/clients').send(getClient);
-        expect(response.status).to.be.equal(201);
+        const chaiHttpResponse = await chai.request(app).post('/clients').send(getClient);
+        expect(chaiHttpResponse.status).to.be.equal(201);
         expect(chaiHttpResponse.body).to.be.deep.equal(getClient);
-    })
+    });
+
+    it('testa status de cliente existente', async function () {
+        sinon
+            .stub(clientModel, "create")
+            .rejects({ message: "Cliente já cadastrado" });
+
+        const chaiHttpResponse = await chai.request(app).post('/clients').send(getClient);
+        expect(chaiHttpResponse.status).to.be.equal(400);
+        expect(chaiHttpResponse.body).to.be.deep.equal({ "message": "Cliente já cadastrado" });
+    });
 });
 
-describe('testa a rota PUT de /clients', async function () {
+describe('testa a rota PUT de /clients', function () {
     afterEach(function () {
         sinon.restore();
     });
@@ -79,23 +76,24 @@ describe('testa a rota PUT de /clients', async function () {
     it('testa se é possível atualizar um cliente', async function () {
         sinon
             .stub(clientModel, "update")
+            .resolves("Dados alterados.");
 
-        chaiHttpResponse = await chai.request(app).put('/clients/1').send(getClient);
-        expect(response.status).to.be.equal(200);
+        const chaiHttpResponse = await chai.request(app).put('/clients/1').send(getClient);
+        expect(chaiHttpResponse.status).to.be.equal(200);
         expect(chaiHttpResponse.body).to.be.deep.equal("Dados alterados.");
     });
-})
+});
 
-describe('testa a rota DELETE de /clients', async function () {
+describe('testa a rota DELETE de /clients', function () {
     afterEach(function () {
         sinon.restore();
     });
 
     it('testa se é possível deletar um cliente', async function () {
         sinon
-            .stub(clientModel, "destroy")
+            .stub(clientModel, "destroy");
 
-        chaiHttpResponse = await chai.request(app).delete('/clients/1');
-        expect(response.status).to.be.equal(204);
-    })
-})
+        const chaiHttpResponse = await chai.request(app).delete('/clients/1');
+        expect(chaiHttpResponse.status).to.be.equal(204);
+    });
+});
